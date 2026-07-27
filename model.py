a4abf0f028113ae4fe8d1032005a223b2095d19a
ZilongZhang <69504465+ZilongZhang-cyber@users.noreply.github.com>
2026-07-27 10:35:52 +0800

    Step 77: select_top_k_candidates

--- model.py
+++ model.py
@@ -871,8 +871,23 @@ def compute_candidate_scores(beam_scores, next_token_log_probs):
     # TODO: add each beam's running log-prob to its row of next-token log probs.
     return beam_scores.unsqueeze(-1) + next_token_log_probs
 
-# Step 77 - select_top_k_candidates (not yet solved)
-# TODO: implement
+# Step 77 - select_top_k_candidates
+import torch
+
+def select_top_k_candidates(candidate_scores, k):
+    # TODO: pick the top k (beam_index, token_id, score) triples from candidate_scores
+    flat_scores = candidate_scores.view(-1)  # (num_beams * vocab_size,)
+    top_scores, top_indices = flat_scores.topk(k)
+
+    vocab_size = candidate_scores.size(-1)
+    beam_indices = top_indices // vocab_size  # 第几个 beam
+    token_ids = top_indices % vocab_size      # 第几个 token
+
+    return {
+        'beam_indices': beam_indices,
+        'token_ids': token_ids,
+        'scores': top_scores
+    }
 
 # Step 78 - append_tokens_to_beam_sequences (not yet solved)
 # TODO: implement
